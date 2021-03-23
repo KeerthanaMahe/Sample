78bc9e68c8eecbaf3951d9f1cb747c924e76fd06
keerthanaMahe <keerthanamarimuthu63@gmail.com>
2021-03-23 17:38:12 +0530

    Firstpush

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,21 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+		System.out.println("cts");
+	}
+	private void companyId() {
+		System.out.println("123");
+	}
+	private void companyAddress() {
+		System.out.println("omr chennai");
+	}
+	public static void main(String[] args) {
+		
+		CompanyInfo c= new CompanyInfo();
+		c.companyName();
+		c.companyId();
+		c.companyAddress();
+	}
+
+}
